7b1af5a9091aa72ed8040a9e9b05f2cd60c3fefa
goemon-github <booya.8108@gmail.com>
2025-03-09 01:13:09 +0900

    modify users table migration

--- ecommerce-app/database/migrations/0001_01_01_000000_create_users_table.php
+++ ecommerce-app/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,10 +15,16 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('country', 100);
+            $table->string('zipcode', 20);
+            $table->text('steet_address');
+            $table->string('city');
+            $table->string('phone', 20);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
