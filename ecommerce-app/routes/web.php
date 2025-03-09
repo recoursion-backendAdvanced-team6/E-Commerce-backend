c6d3df54c59ac5a7387c1297d94a5a686628a102
goemon-github <booya.8108@gmail.com>
2025-03-09 23:57:18 +0900

    create route admin/dashbord

--- ecommerce-app/routes/web.php
+++ ecommerce-app/routes/web.php
@@ -17,4 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/admin/dashbord', function() {
+    return view('admin.dashbord');
+});
+
+
+
 require __DIR__.'/auth.php';
